dedfc914c599176784bea8f9c28721baa3c0e48a
Aaron Serrano <aaronbserrano@yahoo.com>
2017-02-18 17:02:36 +0800

    Initial Commit

---
+++ Rates/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rates
+//
+//  Created by Aaron Serrano on 18/2/17.
+//  Copyright © 2017 LinuxEdge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
